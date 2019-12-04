b90a2f56b3a2e08c4a07507d550bbe917458b87d
Song Gao <song@gao.io>
2019-12-04 15:14:22 -0800

    add allowedTeams in config; add http server for elb health checks

--- jirabot/src/index.ts
+++ jirabot/src/index.ts
@@ -1,6 +1,23 @@
 import Bot from './bot'
 import {init} from './context'
 import * as BotConfig from './bot-config'
+import http from 'http'
+
+const handleElbHealthCheck = () =>
+  http
+    .createServer((req, res) => {
+      try {
+        if (req.url === '/jirabot') {
+          res.write('ok')
+          res.end()
+          return
+        }
+        res.writeHead(404)
+        res.write('not found')
+        res.end()
+      } catch {}
+    })
+    .listen(8080)
 
 const botConfig = BotConfig.parse(process.env.JIRABOT_CONFIG || '')
 if (!botConfig) {
@@ -8,5 +25,6 @@ if (!botConfig) {
   console.error(process.env.JIRABOT_CONFIG)
   process.exit(1)
 } else {
+  handleElbHealthCheck()
   init(botConfig).then(Bot)
 }
