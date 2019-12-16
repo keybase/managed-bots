6e2b20e79fc09fb3b51f091b7d84091606745cbe
Song Gao <song@gao.io>
2019-12-16 10:17:51 -0800

    use jirabot- prefix for oauth callbcak

--- jirabot/src/constants.ts
+++ jirabot/src/constants.ts
@@ -1,4 +1,4 @@
 export const healthCheckPathname = '/jirabot'
-export const jiraOauthCallbackPathname = '/jira-oauth'
+export const jiraOauthCallbackPathname = '/jirabot-oauth'
 export const jiraCallbackTimeout = 300 * 1000 // 5min
 
