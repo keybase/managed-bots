786db81b18dc1b83d3639c4e519c34b3506fb657
Marcel O'Neil <marcel@keyba.se>
2020-01-07 17:55:54 -0500

    meetbot: better errors & use KeybaseLocation (#37)

--- meetbot/main.go
+++ meetbot/main.go
@@ -63,22 +63,22 @@ func (s *BotServer) Go() (err error) {
 		return fmt.Errorf("BOT_KBFS_ROOT must be specified\n")
 	}
 	configPath := filepath.Join(s.opts.KBFSRoot, "credentials.json")
-	cmd := exec.Command("keybase", "fs", "read", configPath)
+	cmd := exec.Command(s.opts.KeybaseLocation, "fs", "read", configPath)
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	fmt.Printf("Running `keybase fs read` on %q and waiting for it to finish...\n", configPath)
 	if err := cmd.Run(); err != nil {
-		return err
+		return fmt.Errorf("could not read credentials.json: %v", err)
 	}
 
 	// If modifying these scopes, drop the saved tokens in the db
 	config, err := google.ConfigFromJSON(out.Bytes(), calendar.CalendarEventsScope)
 	if err != nil {
-		return fmt.Errorf("Unable to parse client secret file to config: %v", err)
+		return fmt.Errorf("unable to parse client secret file to config: %v", err)
 	}
 
 	if s.kbc, err = s.Start(s.opts.KeybaseLocation, s.opts.Home); err != nil {
-		return err
+		return fmt.Errorf("failed to start keybase %v", err)
 	}
 
 	sdb, err := sql.Open("mysql", s.opts.DSN)
