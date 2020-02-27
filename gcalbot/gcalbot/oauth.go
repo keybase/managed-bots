9431a6f2571b1b96bd3d24c185af6a2e87ebffdb
Marcel O'Neil <marcel@keyba.se>
2020-02-27 17:19:26 -0500

    restrict defaults for 1on1 convs

--- gcalbot/gcalbot/oauth.go
+++ gcalbot/gcalbot/oauth.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"net/url"
 
+	"google.golang.org/api/calendar/v3"
+
 	"github.com/keybase/managed-bots/base"
 
 	"github.com/keybase/go-keybase-chat-bot/kbchat/types/chat1"
@@ -74,36 +76,45 @@ func (h *HTTPSrv) oauthHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	conv, err := h.kbc.GetConversation(req.KeybaseConvID)
+	if err != nil {
+		return
+	}
+
+	// if account was created in a 1on1 conv, create default subscription to invites & 5 minute reminder for primary calendar
+	if base.IsDirectPrivateMessage(h.kbc.GetUsername(), req.KeybaseUsername, conv.Channel) {
+		var srv *calendar.Service
+		srv, err = GetCalendarService(&account, h.oauth)
+		if err != nil {
+			return
+		}
+
+		var primaryCalendar *calendar.Calendar
+		primaryCalendar, err = srv.Calendars.Get("primary").Do()
+		if err != nil {
+			return
+		}
+
+		_, err = h.handler.createSubscription(&account, Subscription{
+			CalendarID:    primaryCalendar.Id,
+			KeybaseConvID: req.KeybaseConvID,
+			Type:          SubscriptionTypeInvite,
+		})
+		if err != nil {
+			return
+		}
+		_, err = h.handler.createSubscription(&account, Subscription{
+			CalendarID:     primaryCalendar.Id,
+			KeybaseConvID:  req.KeybaseConvID,
+			DurationBefore: GetDurationFromMinutes(5),
+			Type:           SubscriptionTypeReminder,
+		})
+		if err != nil {
+			return
+		}
+	}
+
 	loginToken := h.handler.LoginToken(req.KeybaseUsername)
-
-	srv, err := GetCalendarService(&account, h.oauth)
-	if err != nil {
-		return
-	}
-
-	primaryCalendar, err := srv.Calendars.Get("primary").Do()
-	if err != nil {
-		return
-	}
-
-	// create default subscription to invites & 5 minute reminder for primary calendar
-	_, err = h.handler.createSubscription(&account, Subscription{
-		CalendarID:    primaryCalendar.Id,
-		KeybaseConvID: req.KeybaseConvID,
-		Type:          SubscriptionTypeInvite,
-	})
-	if err != nil {
-		return
-	}
-	_, err = h.handler.createSubscription(&account, Subscription{
-		CalendarID:     primaryCalendar.Id,
-		KeybaseConvID:  req.KeybaseConvID,
-		DurationBefore: GetDurationFromMinutes(5),
-		Type:           SubscriptionTypeReminder,
-	})
-	if err != nil {
-		return
-	}
 
 	redirectQuery := url.Values{}
 	redirectQuery.Add("token", loginToken)
