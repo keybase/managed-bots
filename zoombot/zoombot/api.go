f32e1395402eb9271dbe9d74c4d8ad0a53415385
Marcel O'Neil <marcel@keyba.se>
2020-03-19 12:03:27 -0400

    zoombot: avoid parsing extra user data

--- zoombot/zoombot/api.go
+++ zoombot/zoombot/api.go
@@ -53,32 +53,10 @@ type GlobalDialInNumbers struct {
 
 // Get User
 
+// intentionally leave out unnecessary user data
 type GetUserResponse struct {
-	ID                 string        `json:"id"`
-	FirstName          string        `json:"first_name"`
-	LastName           string        `json:"last_name"`
-	Email              string        `json:"email"`
-	Type               int           `json:"type"`
-	RoleName           string        `json:"role_name"`
-	Pmi                int           `json:"pmi"`
-	UsePmi             bool          `json:"use_pmi"`
-	PersonalMeetingURL string        `json:"personal_meeting_url"`
-	Timezone           string        `json:"timezone"`
-	Verified           int           `json:"verified"`
-	Dept               string        `json:"dept"`
-	CreatedAt          time.Time     `json:"created_at"`
-	LastLoginTime      time.Time     `json:"last_login_time"`
-	LastClientVersion  string        `json:"last_client_version"`
-	PicURL             string        `json:"pic_url"`
-	HostKey            string        `json:"host_key"`
-	Jid                string        `json:"jid"`
-	GroupIds           []interface{} `json:"group_ids"`
-	ImGroupIds         []string      `json:"im_group_ids"`
-	AccountID          string        `json:"account_id"`
-	Language           string        `json:"language"`
-	PhoneCountry       string        `json:"phone_country"`
-	PhoneNumber        string        `json:"phone_number"`
-	Status             string        `json:"status"`
+	ID        string `json:"id"`
+	AccountID string `json:"account_id"`
 }
 
 // Create Meeting
