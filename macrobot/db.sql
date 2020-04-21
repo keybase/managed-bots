fcc9e7b5beb7fe620a5fc8c7bba3cbf411eb54db
Joshua Blum <jblum18@gmail.com>
2020-04-21 10:44:58 -0400

    get out of here null

--- macrobot/db.sql
+++ macrobot/db.sql
@@ -3,6 +3,6 @@ CREATE TABLE `macro` (
   `macro_name` varchar(128) NOT NULL,
   `macro_message` varchar(10000) NOT NULL,
   -- NOTE: if `is_conv` is set, `channel_name` holds a `conversation_id`
-  `is_conv` BOOLEAN DEFAULT FALSE,
+  `is_conv` BOOLEAN DEFAULT FALSE NOT NULL,
   PRIMARY KEY (`channel_name`, `macro_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
