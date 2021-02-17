173587795cb82fb06d0d73c03159e56276e47968
Song Gao <song@gao.io>
2021-02-16 21:58:46 -0800

    try to send logs when unhanlded promise rejection happens

--- jirabot/src/index.ts
+++ jirabot/src/index.ts
@@ -6,21 +6,24 @@ import startHTTPServer from './http-server'
 import startBackgroundTasks from './background-tasks'
 import logger from './logger'
 
-process.on('unhandledRejection', (reason, promise) => {
-  logger.fatal({
-    msg: 'unhandled promise rejection',
-    reason: reason?.toString(),
-    promise: util.format(promise),
-  })
-  process.exit(1)
-})
-
 const botConfig = BotConfig.parse(process.env.JIRABOT_CONFIG || '')
 if (!botConfig) {
   logger.fatal('invalid bot-config')
   process.exit(1)
 } else {
   init(botConfig).then(context => {
+    process.on('unhandledRejection', (reason, promise) => {
+      logger.error({
+        msg: 'unhandled promise rejection',
+        reason: reason?.toString(),
+        promise: util.format(promise),
+      })
+      logger.info({
+        msg: 'sending log ...',
+      })
+      context.bot.logSend().then(() => process.exit(1))
+    })
+
     startHTTPServer(context)
     startBackgroundTasks(context)
     Bot(context)
