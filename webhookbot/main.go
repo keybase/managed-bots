9c44462d37c4cf4cfe7a72924a7acea5aa6ad636
zoom-ua <65734190+zoom-ua@users.noreply.github.com>
2026-07-02 12:36:53 -0400

    reject RO param to mysql conn (#347)

--- webhookbot/main.go
+++ webhookbot/main.go
@@ -102,7 +102,7 @@ func (s *BotServer) Go() (err error) {
 	if s.kbc, err = s.Start(s.opts.ErrReportConv); err != nil {
 		return err
 	}
-	sdb, err := sql.Open("mysql", s.opts.DSN)
+	sdb, err := sql.Open("mysql", s.opts.DSN.FormatDSN())
 	if err != nil {
 		s.Errorf("failed to connect to MySQL: %s", err)
 		return err
@@ -149,7 +149,7 @@ func mainInner() int {
 		fmt.Printf("Unable to parse options: %v\n", err)
 		return 3
 	}
-	if len(opts.DSN) == 0 {
+	if opts.DSN == nil {
 		fmt.Printf("must specify a database DSN\n")
 		return 3
 	}
